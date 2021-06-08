94d02b0f4497ed4e0667ef169a80fd41d5170813
Johannes Hauschild <git@johannes-hauschild.de>
2021-06-08 13:11:13 -0700

    add git transcrypt and gitconfig

--- git/bootstrap.sh
+++ git/bootstrap.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 set -e 
+echo "bootstrap for git installation"
 
 git submodule update .
+
 DIR="$(realpath $(dirname $0))"
 echo "DIR=$DIR"
-test -f $DIR/gitstatus/gitstatus.prompt.sh
-echo "found gitstatus"
-(
-	echo '#!/bin/bash'
-	echo 'GITSTATUS_DIR="'"$DIR"'/gitstatus/gitstatus.plugin.sh"'
-	echo 'source "'"$DIR"'/gitstatus/gitstatus.prompt.sh"'
-) > $DIR/src/dot-bashrc-source/gitstatus.sh
-echo "created gitstatus.sh"
+if [ -f $DIR/gitstatus/gitstatus.prompt.sh ]
+then
+	echo "found gitstatus"
+	(
+		echo '#!/bin/bash'
+		echo 'GITSTATUS_DIR="'"$DIR"'/gitstatus/gitstatus.plugin.sh"'
+		echo 'source "'"$DIR"'/gitstatus/gitstatus.prompt.sh"'
+	) > $DIR/src/dot-bashrc-source/gitstatus.sh
+	echo "created gitstatus.sh"
+fi
+
