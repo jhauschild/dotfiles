f71701559e06cde3d6a191ccaddf02916aea5fdd
Johannes Hauschild <git@johannes-hauschild.de>
2022-08-29 08:37:59 +0200

    rofi pass.sh script

---
+++ wm_i3_sway/src/dot-config/rofi/scripts/pass.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# based on https://git.zx2c4.com/password-store/tree/contrib/dmenu/passmenu
+# extended by JHauschild to use rofi instead
+# use as `rofi -show power -modi "power:~/.config/rofi/scripts/pass.sh"`
+# `rofi -show power -modi "power:~/.config/rofi/scripts/pass.sh --type"`
+
+
+shopt -s nullglob globstar
+
+typeit=0
+if [[ $1 == "--type" ]]; then
+	typeit=1
+	shift
+fi
+
+
+
+prefix=${PASSWORD_STORE_DIR-~/.password-store}
+password_files=( "$prefix"/**/*.gpg )
+password_files=( "${password_files[@]#"$prefix"/}" )
+password_files=( "${password_files[@]%.gpg}" )
+
+if [[ -z "$1" ]]; then
+	# rofi calls script once without args to get options
+	printf '%s\n' "${password_files[@]}"
+	exit 0  
+fi 
+# and second time with selection
+password="$1"
+
+[[ -n $password ]] || exit
+
+if  [[ -n "WAYLAND_DISPLAY" ]] ; then
+TYPE="wtype"
+else
+TYPE="xdotool type --clearmodifiers --file "
+fi
+
+if [[ $typeit -eq 0 ]]; then
+	pass show -c "$password" &>/dev/null
+else
+	pass show "$password" | { IFS= read -r pass; printf %s "$pass"; } | $TYPE -
+fi
+exit 0
